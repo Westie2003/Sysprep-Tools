f3d7bf77cea510b8399d5197eb729462a24f0d02
Westie2003 <91673724+Westie2003@users.noreply.github.com>
2024-10-06 23:52:42 +0200

    Add files via upload

---
+++ Lokaler_admin_kannste.kk.ps1
@@ -0,0 +1,8 @@
+﻿:: ===========================================================================
+:: Script: Lokales Admin-Konto erstellen
+:: Autor: @kannste.kk (Discord)
+:: Datum: 01.10.2024
+:: Beschreibung: Erstellt ein zusätzliches administratives Konto für z.B. Schul-Admins
+:: ===========================================================================
+New-LocalUser -Name "Admin" -Password (ConvertTo-SecureString "PasswortmitspeziellemZeichenÁÓ" -AsPlainText -Force)
+Add-LocalGroupMember "Administratoren" "Admin"
---
+++ SystemUser_aktivieren_kannste.kk.ps1
@@ -0,0 +1,9 @@
+﻿:: ===========================================================================
+:: Script: Admin-Konto-Passwort-Änderung und Aktivierung
+:: Autor: @kannste.kk (Discord)
+:: Datum: 01.10.2024
+:: Beschreibung: Aktiviert das Administrator-Konto und erstellt ein neues Kennwort
+:: ===========================================================================
+$User = Get-LocalUser "Administrator"
+Set-LocalUser $User -Password (ConvertTo-SecureString "ÁNTUONIÓsicherespassworthier" -AsPlainText -Force)
+Enable-LocalUser $User
